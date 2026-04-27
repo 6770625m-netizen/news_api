29cefdc59124e28a8291d31f2428b51fa41d7c5b
sultanov_00 <6770625m@gmail.com>
2026-04-28 01:00:36 +0500

    first commit

--- news_project/settings.py
+++ news_project/settings.py
@@ -63,20 +63,14 @@ if DB_ENGINE == 'django.db.backends.postgresql':
     DATABASES = {
         'default': {
             'ENGINE': DB_ENGINE,
-            'NAME': config('DB_NAME', default='news_db'),
-            'USER': config('DB_USER', default='postgres'),
-            'PASSWORD': config('DB_PASSWORD', default='postgres123'),
+            'NAME': config('POSTGRES_DB', default='news_db'),
+            'USER': config('POSTGRES_USER', default='postgres'),
+            'PASSWORD': config('POSTGRES_PASSWORD', default='postgres123'),
             'HOST': config('DB_HOST', default='localhost'),
             'PORT': config('DB_PORT', default='5432'),
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
 
 AUTH_USER_MODEL = 'accounts.User'
 
